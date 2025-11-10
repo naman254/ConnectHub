92ab223b94341be900f4f3dfdccd4a322a20a24e
Naman  Singh <naman.singh.254@gmail.com>
2025-11-10 19:54:11 +0530

    update profile endpoint added

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,7 @@ import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
+import cookieParser from "cookie-parser";
 
 
 
@@ -13,6 +14,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
