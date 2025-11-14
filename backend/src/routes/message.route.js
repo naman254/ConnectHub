57a19101c2069614d2bd29215c3aa9c4bacb3bb4
Naman  Singh <naman.singh.254@gmail.com>
2025-11-14 23:50:22 +0530

    message routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
+import { getAllContacts } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
-router.get("/send", (req,res)=>{
-    res.send("Send message endpoint");
-});
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts",protectRoute, getAllContacts);
+router.get("/chats", protectRoute, getChatPartners);
+router.get("/:id",protectRoute, getMessagesByUserId);
+
+router.post("/send/:id",protectRoute, sendMessage);
 
 export default router;
